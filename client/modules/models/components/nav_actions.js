d93d8e6a13f032f60dc8fa24d8206c4382731927
shoetten <simon@hoetten.org>
2016-05-03 21:31:01 +0200

    Start Add Model functionality for models

--- client/modules/models/components/nav_actions.js
+++ client/modules/models/components/nav_actions.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const NavActions = () => (
-  <ul className="right">
+  <ul>
     <li>
       <a className="dropdown-button" href="#" data-activates="dropdown1">
         <i className="material-icons right">more_vert</i>
