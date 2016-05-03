5c3bd06aa3ca9c35550cbdddb1590fb88fc37368
shoetten <simon@hoetten.org>
2016-05-03 21:18:07 +0200

    Update react hot loader to v3.

--- client/main.js
+++ client/main.js
@@ -2,11 +2,14 @@
  * https://kadirahq.github.io/mantra/
  */
 
+import 'react-hot-loader/patch';
+
 import {createApp} from 'mantra-core';
 import initContext from '/client/configs/context';
 
 // import all modules here
 import coreModule from '/client/modules/core';
+import users from '/client/modules/users';
 import home from '/client/modules/home';
 import models from '/client/modules/models';
 
@@ -16,6 +19,7 @@ const context = initContext();
 // create app
 const app = createApp(context);
 app.loadModule(coreModule);
+app.loadModule(users);
 app.loadModule(home);
 app.loadModule(models);
 app.init();
