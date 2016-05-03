636cf48ef217fe103d2349cd1cdff03be4eba2e4
shoetten <simon@hoetten.org>
2016-05-03 21:30:28 +0200

    remove NavActions from home module

--- client/modules/home/routes.js
+++ client/modules/home/routes.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {mount} from 'react-mounter';
 
 import MainLayout from '../core/components/main_layout';
-import NavActions from './components/nav_actions';
 import Home from './components/home';
 
 export default function (injectDeps, {FlowRouter}) {
@@ -12,7 +11,6 @@ export default function (injectDeps, {FlowRouter}) {
     name: 'home',
     action() {
       mount(MainLayoutCtx, {
-        NavActions,
         content: () => (<Home />),
       });
     },
