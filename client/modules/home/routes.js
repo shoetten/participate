2ce1b888ab659f032e62fed1d988c07e82c112fb
shoetten <simon@hoetten.org>
2016-05-06 14:27:27 +0200

    Try out react hot loader with gadicc:meteor-hmr.

--- client/modules/home/routes.js
+++ client/modules/home/routes.js
@@ -1,18 +1,46 @@
 import React from 'react';
 import {mount} from 'react-mounter';
+import {AppContainer} from 'react-hot-loader';
 
-import MainLayout from '../core/components/main_layout';
-import Home from './components/home';
+// import MainLayout from '../core/components/main_layout';
+// import Home from './components/home';
+
+let localFlowRouter = null;
 
 export default function (injectDeps, {FlowRouter}) {
-  const MainLayoutCtx = injectDeps(MainLayout);
+  localFlowRouter = FlowRouter;
+
+  // const MainLayoutCtx = injectDeps(MainLayout);
+  const MainLayoutCtx = function (props) {
+    const MainLayout = require('../core/components/main_layout').default;
+    const MainLayoutCtx = injectDeps(MainLayout);
+    return (
+      <AppContainer>
+        <MainLayoutCtx {...props} />
+      </AppContainer>
+    );
+  };
 
   FlowRouter.route('/', {
     name: 'home',
     action() {
+      const Home = require('./components/home').default;
       mount(MainLayoutCtx, {
         content: () => (<Home />),
       });
     },
+  });
+}
+
+if (module.hot) {
+  module.hot.accept([
+    '../core/components/main_layout',
+    './containers/home',
+  ], () => {
+    // If any of the above files (or their dependencies) are updated, all we
+    // really need to do is re-run the current route's action() method, which
+    // will require() the updated modules and re-mount MainLayoutCtx
+    // (which itself require()'s the updated MainLayout at render time).
+    localFlowRouter._current.route._action();
   });
 }
