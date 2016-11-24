9a34bf0e3aed59073bec0a214fc62aa800247dc0
shoetten <simon@hoetten.org>
2016-11-24 15:47:25 +0100

    Remove react-tap-event-plugin, because it's not really needed any more with newer browsers.

--- app/modules/core/index.js
+++ app/modules/core/index.js
@@ -1,6 +1,5 @@
 // This has to be loaded first, in order to get HMR to work
 import 'react-hot-loader/patch';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import routes from './routes';
 import actions from './actions';
 import setHeader from './configs/header';
@@ -11,9 +10,5 @@ export default {
   load(context) {
     // init module here
     setHeader(context);
-
-    // Needed for onTouchTap
-    // http://stackoverflow.com/a/34015469/988941
-    injectTapEventPlugin();
   },
 };
