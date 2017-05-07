2bd1a8f3760b48fb761e511f7e26fcc5ed9876c4
shoetten <simon@hoetten.org>
2017-05-07 22:50:13 +0200

    Rewrite loading component.

--- app/modules/core/components/loading.js
+++ app/modules/core/components/loading.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import Loading from 'react-loading';
+import PropTypes from 'prop-types';
+import CircularProgress from 'material-ui/CircularProgress';
 
-const LoadingComponent = () => (
-  <div className="loading">
-    <Loading type="balls" color="inherit" delay={500} />
-  </div>
+const LoadingComponent = ({size, thickness, style}) => (
+  <CircularProgress size={size} thickness={thickness} style={style} />
 );
+
+LoadingComponent.propTypes = {
+  size: PropTypes.number,
+  thickness: PropTypes.number,
+  style: PropTypes.object,
+};
+LoadingComponent.defaultProps = {
+  size: 60,
+  thickness: 7,
+  style: {display: 'block', margin: '0 auto'},
+};
 
 export default LoadingComponent;
