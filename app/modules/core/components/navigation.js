36a4bf590d0b02f7eac7a52e25358be3655458a5
shoetten <simon@hoetten.org>
2017-05-07 11:57:54 +0200

    Remove beta warning from header.

--- app/modules/core/components/navigation.js
+++ app/modules/core/components/navigation.js
@@ -37,10 +37,6 @@ class Navigation extends React.Component {
               </a>
             }
 
-            <a href={pathFor('beta')} className="brand-logo center beta-warning">
-              <i className="material-icons left">warning</i> Beta
-            </a>
-
             {currentUser ?
               <ul className="right" key="loggedInControls">
                 <li className="nav-actions">{children}</li>
